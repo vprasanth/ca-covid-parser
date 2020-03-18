db3d1b389f72dba34c1357713ae6267d2d11e7ea
Prasanth Vaaheeswaran <pvaahee@ca.ibm.com>
2020-03-18 12:20:09 -0400

    refactor logic in to modules

--- index.js
+++ index.js
@@ -1,85 +1,19 @@
-import https from 'https';
 import process from 'process';
-import cheerio from 'cheerio';
-import cc from 'camelcase';
 
-// const URL = 'https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html';
+import {fetchPage, parsePage} from './lib/index.mjs';
+
 const url = process.argv[2];
-
 if (!url) {
+  throw new Error('URL required!');
   process.exit(1);
-} else {
-  init(url);
 }
 
-function init(url) {
-  https
-    .get(url, res => {
-      let data = '';
+init(url)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
 
-      res.on('data', chunk => {
-        data += chunk;
-      });
-
-      res.on('end', () => {
-        // console.log(data);
-        const result = make(data);
-        if (result[0].length) {
-          console.log(result);
-        } else {
-          console.log('No data for', url);
-        }
-      });
-    })
-    .on('error', err => {
-      console.log(err);
-      process.exit(1);
-    });
-}
-
-function make(data) {
-  let tableSelector = '';
-  let dateRegex = /.*As of (\w.*)\s(\d*),\s(\d*),/i;
-  const $ = cheerio.load(data);
-
-  if ($('div.table-responsive').length) {
-    // old
-    tableSelector = 'div.table-responsive > table';
-  } else {
-    tableSelector = 'main > div > table';
-  }
-
-  const table = $(tableSelector).first();
-
-  const match = dateRegex.exec($('body').text());
-  const date = new Date(
-    `${match[1]} ${match[2]} ${match[3]} 00:00:00`,
-  ).toISOString();
-
-  const headings = table.find('thead > tr > th');
-  let keys;
-  if (headings.length === 3) {
-    keys = ['location', 'confirmed', 'probable'];
-  } else {
-    keys = ['location', 'confirmed'];
-  }
-
-  const body = table.find('tbody > tr');
-
-  const cases = [];
-  body.each(function(elm) {
-    let report = {};
-    $(this)
-      .find('td')
-      .each(function(i, elm) {
-        if (i === 0) {
-          report[keys[i]] = cc($(this).text());
-        } else {
-          report[keys[i]] = parseInt($(this).text(), 10);
-        }
-      });
-    cases.push(report);
-  });
-
-  return [cases, date];
+async function init(url) {
+  const page = await fetchPage(url);
+  const data = parsePage(page);
+  return data;
 }
